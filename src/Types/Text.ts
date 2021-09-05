be9245db4c168ba299d5956515aa4a22b6480233
shrivatsaBhat <shrivatsabhatpolali@gmail.com>
2021-09-05 18:12:53 +0530

    chore: add the Types

---
+++ src/Types/Text.ts
@@ -0,0 +1,39 @@
+export const TextTypes = {
+  TEXT: {
+    TITLE: 'TEXT_TITLE',
+    BODY: 'TEXT_BODY',
+    SECTION: 'TEXT_SECTION',
+    SUBTITLE: 'TEXT_SUBTITLE',
+    SIZE: {
+      TITLE: {
+        XL: 'XL',
+        L: 'L',
+        M: 'M',
+        S: 'S',
+        XS: 'XS',
+      },
+      BODY: {
+        L: 'L',
+        M: 'M',
+        S: 'S',
+      },
+      SECTION: {
+        XXL: 'XXL',
+        XL: 'XL',
+        L: 'L',
+        M: 'M',
+        S: 'S',
+        XS: 'XS',
+        XXS: 'XXS',
+      },
+      SUBTITLE: {
+        L: 'L',
+        M: 'M',
+        S: 'S',
+      },
+    },
+    SEPARATOR: {
+      DOT: '•',
+    },
+  },
+};
