7f4024e2ff1d138b548f0266f7af6b3cbc1ee421
shrivatsaBhat <shrivatsabhatpolali@gmail.com>
2021-09-05 18:08:20 +0530

    feat: add assets

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -1,4 +1,13 @@
-import toggleIcon from './recycle.png';
+import toggleIcon from './images/arrow-rotate.png';
+import logo from './product/logo200X200.png';
+import headerLogo from './product/logo350X150.png';
+import plus from './images/plus-lg.png';
+import minus from './images/minus.png';
+
 export const Icon = {
   toggleIcon,
+  plus,
+  minus,
 };
+
+export default { logo, headerLogo };
